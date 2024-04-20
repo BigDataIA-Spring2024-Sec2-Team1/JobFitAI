f993ac8bb8a9dfe378e18e688c757966afaac29e
Aniketgiram <giram.a@northeastern.edu>
2024-04-20 17:03:50 -0400

    knowledge base

---
+++ constants.py
@@ -0,0 +1,15 @@
+RESUME_SECTIONS_GRAD = [
+    'accomplishments',
+    'experience',
+    'education',
+    'interests',
+    'projects',
+    'professional experience',
+    'publications',
+    'skills',
+    'certifications',
+    'objective',
+    'career objective',
+    'summary',
+    'leadership'
+]
