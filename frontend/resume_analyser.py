fd5ee06a52c92c6db32b94a2ea63a912f0d2963e
rasika <kole.r@northeastern.edu>
2024-04-25 22:28:57 -0400

    Frontend resume analyser updated

--- frontend/resume_analyser.py
+++ frontend/resume_analyser.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import requests
 import json
-
+import pandas as pd
 def resume_analyser():
 
     url = "http://localhost:8000"
-
+    
     if 'user_resume_keywords' not in st.session_state:
         st.session_state.user_resume_keywords = []
 
@@ -15,12 +15,17 @@ def resume_analyser():
     if 'user_designations' not in st.session_state:
         st.session_state.user_designations = []
 
-    if 'jd_match_skills' not in st.session_state:
-        st.session_state.jd_match_skills = []
-
     if 'resume_text' not in st.session_state:
         st.session_state.resume_text = ''
 
+    st.markdown("<h1 style='text-align: center;'>Resume Analyser</h1>", unsafe_allow_html=True)
+    col1, col2, col3 = st.columns([1, 4, 1])
+    with col1:
+        pass
+    with col2:
+        st.image("Resume_Analyser.jpg", width=300, use_column_width=True)
+    with col3:
+        pass
     uploaded_file = st.file_uploader("Upload a file", type=['docx', 'pdf'])
 
     if uploaded_file:
@@ -39,7 +44,6 @@ def resume_analyser():
                     user_categorization_res = requests.post(f"{url}/user-categorization", json={"skills": parse_resume_res.json().get("skills"), "namespace": "resume_skills", "resume_text": parse_resume_res.json().get("text")})
                     if user_categorization_res.status_code == 200:
                         st.session_state.similar_user_keywords.append(user_categorization_res.json().get("similar_profile_skills"))
-                        st.session_state.jd_match_skills.append(user_categorization_res.json().get("jd_skills"))
                         st.toast("Resume Parsing and User Categorization Successfull")
                         st.session_state.user_designations = user_categorization_res.json().get("designations")
                         st.session_state.user_designations.append("Other")
@@ -49,35 +53,34 @@ def resume_analyser():
                     st.error("Failed in Parsing resume")
             else:
                 st.error("Failed to upload file. Error: {}".format(response.text))
-
+    
         if 'selected_designation' not in st.session_state:
             st.session_state.selected_designation = ''
 
         st.session_state.selected_designation = st.selectbox("Select the position you are targeting to apply", st.session_state.user_designations, key='selected_designation_key')
-
+        
         if st.session_state.selected_designation == "Other":
             user_input = st.text_input("If targeting position is not in the list, type below.")
             st.write(user_input)
             st.session_state.selected_designation = user_input
-
+        
         if st.session_state.selected_designation:
             if st.button("Suggest Keywords"):
-                getSkillsSuggestion(url, st.session_state.user_resume_keywords, st.session_state.similar_user_keywords, st.session_state.jd_match_skills)
+                getSkillsSuggestion(url, st.session_state.user_resume_keywords, st.session_state.similar_user_keywords)
 
-
+        
         # st.write(f"Intersection - {set_b.union(set_a)}")
         # st.write("Already present", set_a.intersection())
+        
 
+def getSkillsSuggestion(url, user_resume_keywords, similar_user_keywords):
 
-def getSkillsSuggestion(url, user_resume_keywords, similar_user_keywords, jd_skills):
-
-    if user_resume_keywords and similar_user_keywords:
+    if user_resume_keywords:
         flat_a = [item.lower() for sublist in user_resume_keywords for item in sublist]
         flat_b = [item.lower() for sublist in similar_user_keywords for subsublist in sublist for item in subsublist]
-        jd_skills = [item.lower() for sublist in jd_skills for subsublist in sublist for item in subsublist.split(",")]
+            
         set_a = set(flat_a)
         set_b = set(flat_b)
-        set_jd_skills = set(jd_skills)
         low_priority_skills = list(set_a.union(set_b) - set_a.intersection(set_b))
         intersection_result = set_a.intersection(set_b)
         only_set_b = set_b.difference(set_a)
@@ -85,7 +88,7 @@ def getSkillsSuggestion(url, user_resume_keywords, similar_user_keywords, jd_ski
 
         # if you want totally new keywords which are not in resume then pass only_set_b in similar_skills or else combined one will also set some of the relevent skills which might be present in resume
         suggested_skills = requests.post(f"{url}/suggest-keyword", json={"similar_skills": list(combined_result), "resume_text": st.session_state.resume_text, "designation": st.session_state.selected_designation, "num_of_skills": 20})
-
+            
         if suggested_skills.status_code == 200:
             ai_suggested_skills = [item.lower() for item in eval(suggested_skills.json().get("response")).get("Skills")]
             top_k_skills = [item.lower() for item in eval(suggested_skills.json().get("top_k")).get("Skills")]
@@ -94,7 +97,7 @@ def getSkillsSuggestion(url, user_resume_keywords, similar_user_keywords, jd_ski
                 st.write(ai_suggested_skills)
 
             st.write("Top skills")
-
+            
             num_of_columns = 3
 
             # Create a list of columns
@@ -103,24 +106,14 @@ def getSkillsSuggestion(url, user_resume_keywords, similar_user_keywords, jd_ski
             # Iterate over the list of skills and create a button in each column
             for i, skill in enumerate(top_k_skills):
                 with columns[i % num_of_columns]:
-                    st.button(skill, key=f"{i}{skill}")
-
-
-            st.write(f"Skills we found in Job Posting posted for title: {st.session_state.selected_designation} ")
-
-            jd_columns = st.columns(num_of_columns)
-
-            # Iterate over the list of skills and create a button in each column
-            for i, skill in enumerate(set_jd_skills):
-                with jd_columns[i % num_of_columns]:
-                    st.button(skill, key=f"{i}{skill}")
+                    st.button(skill)
 
             unique = [item for item in low_priority_skills if item not in top_k_skills][1:15]
             st.write("We found these skills that might be applicable to your resume")
             columns_for_unique = st.columns(num_of_columns)
             for i, skill in enumerate(unique):
                 with columns_for_unique[i % num_of_columns]:
-                    st.button(skill, key=f"{i}{skill}")
-
+                    st.button(skill)
+            
     else:
         st.toast("Sommething went wrong")
